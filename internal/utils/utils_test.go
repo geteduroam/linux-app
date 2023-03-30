e59d13b3b6e33b3f323a3ac74a66861db7f3a14c
Martin van Es <martin@mrvanes.com>
2023-03-30 17:20:37 +0200

    Add utils package test

---
+++ internal/utils/utils_test.go
@@ -0,0 +1,29 @@
+package utils
+
+import "testing"
+
+func Test_RemoveDiacritics(t *testing.T) {
+	cases := []struct {
+		input string
+		want  string
+		e     error
+	}{
+		{
+			input: "foobar",
+			want:  "foobar",
+			e:     nil,
+		},
+		{
+			input: "fòóbår",
+			want:  "foobar",
+			e:     nil,
+		},
+	}
+
+	for _, c := range cases {
+		result, e := RemoveDiacritics(c.input)
+		if result != c.want || e != c.e {
+			t.Fatalf("Result: %s, %v Want: %s, %v", result, e, c.want, c.e)
+		}
+	}
+}
