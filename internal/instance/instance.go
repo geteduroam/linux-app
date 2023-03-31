f01ecfcee13ab6841940a319b055a11467e38427
jwijenbergh <jeroenwijenbergh@protonmail.com>
2023-03-31 10:49:31 +0200

    Instance: Convert anonymous struct slice to concrete type

--- internal/instance/instance.go
+++ internal/instance/instance.go
@@ -6,13 +6,15 @@ import (
 	"gitlab.geant.org/TI_Incubator/geteduroam-linux/internal/utils"
 )
 
+type geo struct {
+	lat  float32 `json:"lat"`
+	long float32 `json:"long"`
+}
+
 type Instance struct {
-	CatIDP  int    `json:"cat_idp"`
-	Country string `json:"country"`
-	Geo     []struct {
-		lat  float32
-		long float32
-	} `json:"geo"`
+	CatIDP   int       `json:"cat_idp"`
+	Country  string    `json:"country"`
+	Geo      []geo     `json:"geo"`
 	ID       string    `json:"id"`
 	Name     string    `json:"name"`
 	Profiles []Profile `json:"profiles"`
