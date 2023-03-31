ac29b8a7995ec1c51ff0b0b114197c2f1ca4a922
jwijenbergh <jeroenwijenbergh@protonmail.com>
2023-03-31 11:16:26 +0200

    Inner: Convert types to CamelCase

--- internal/network/inner/inner.go
+++ internal/network/inner/inner.go
@@ -9,21 +9,21 @@ type Type int
 
 // TODO: Should we split these in EAP and non-EAP instead?
 const (
-	NONE     Type = 0
-	PAP      Type = 1
-	MSCHAP   Type = 2
-	MSCHAPV2 Type = 3
+	None     Type = 0
+	Pap      Type = 1
+	Mschap   Type = 2
+	Mschapv2 Type = 3
 	// TODO: remove this? https://github.com/geteduroam/windows-app/blob/f11f00dee3eb71abd38537e18881463f83b180d3/CHANGELOG.md?plain=1#L34
-	EAP_PEAP_MSCHAPV2 Type = 25
-	EAP_MSCHAPV2      Type = 26
+	EapPeapMschapv2 Type = 25
+	EapMschapv2      Type = 26
 )
 
 // EAP returns whether the type is an EAP inner type
 func (t Type) EAP() bool {
 	switch t {
-	case EAP_PEAP_MSCHAPV2:
+	case EapPeapMschapv2:
 		return true
-	case EAP_MSCHAPV2:
+	case EapMschapv2:
 		return true
 	}
 	return false
@@ -32,15 +32,15 @@ func (t Type) EAP() bool {
 // String returns the string representation of the inner type
 func (t Type) String() string {
 	switch t {
-	case PAP:
+	case Pap:
 		return "pap"
-	case MSCHAP:
+	case Mschap:
 		return "mschap"
-	case MSCHAPV2:
+	case Mschapv2:
 		fallthrough
-	case EAP_PEAP_MSCHAPV2:
+	case EapPeapMschapv2:
 		fallthrough
-	case EAP_MSCHAPV2:
+	case EapMschapv2:
 		return "mschapv2"
 	}
 	return ""
@@ -62,13 +62,13 @@ func Valid(mt method.Type, input int, eap bool) bool {
 	// For TTLS, we support PAP, MSCHAP, MSCHAPv2 and EAP MSCHAPV2
 	if mt == method.TTLS {
 		switch Type(input) {
-		case PAP:
+		case Pap:
 			return true
-		case MSCHAP:
+		case Mschap:
 			return true
-		case MSCHAPV2:
+		case Mschapv2:
 			return true
-		case EAP_MSCHAPV2:
+		case EapMschapv2:
 			return true
 		}
 		return false
@@ -76,9 +76,9 @@ func Valid(mt method.Type, input int, eap bool) bool {
 	// for PEAP, we only support EAP*MSCHAPV2
 	if mt == method.PEAP {
 		switch Type(input) {
-		case EAP_PEAP_MSCHAPV2:
+		case EapPeapMschapv2:
 			return true
-		case EAP_MSCHAPV2:
+		case EapMschapv2:
 			return true
 		}
 		return false
