98084d1a9602e9fa3c1d5d4a74bd9e3c1e9352e5
Martin van Es <martin@mrvanes.com>
2023-04-06 12:31:37 +0200

    Add Profile tests

--- internal/instance/instance_test.go
+++ internal/instance/instance_test.go
@@ -85,3 +85,96 @@ func Test_Filter(t *testing.T) {
 		}
 	}
 }
+
+func Test_Flow(t *testing.T) {
+	p := Profile{
+		AuthorizationEndpoint: "https://instance1.geteduroam.nl/oauth/authorize/",
+		Default:               true,
+		EapConfigEndpoint:     "https://instance1.geteduroam.nl/api/eap-config/",
+		ID:                    "letswifi_cat_0001",
+		Name:                  "geteduroam",
+		OAuth:                 true,
+		TokenEndpoint:         "https://instance1.geteduroam.nl/oauth/token/",
+		Redirect:              "https://instance1.geteduroam.nl/",
+	}
+
+	var flow FlowCode
+	flow = p.Flow()
+	if flow != RedirectFlow {
+		t.Fatalf("Flow should be RedirectFlow")
+	}
+
+	p.Redirect = ""
+	flow = p.Flow()
+	if flow != OAuthFlow {
+		t.Fatalf("Flow should be OAuthFlow")
+	}
+
+	p.OAuth = false
+	flow = p.Flow()
+	if flow != DirectFlow {
+		t.Fatalf("Flow should be DirectFlow")
+	}
+
+}
+
+func Test_RedirectURI(t *testing.T) {
+	EtoString := func(e error) string {
+		if e != nil {
+			return e.Error()
+		} else {
+			return ""
+		}
+	}
+
+	p := Profile{
+		AuthorizationEndpoint: "https://instance1.geteduroam.nl/oauth/authorize/",
+		Default:               true,
+		EapConfigEndpoint:     "https://instance1.geteduroam.nl/api/eap-config/",
+		ID:                    "letswifi_cat_0001",
+		Name:                  "geteduroam",
+		OAuth:                 true,
+		TokenEndpoint:         "https://instance1.geteduroam.nl/oauth/token/",
+		Redirect:              "",
+	}
+
+	cases := []struct {
+		input string
+		want  string
+		e     string
+	}{
+		{
+			// Normal test
+			input: "https://instance1.geteduroam.nl/",
+			want:  "https://instance1.geteduroam.nl/",
+			e:     "",
+		},
+		{
+			// No Redirect
+			input: "",
+			want:  "",
+			e:     "No redirect found",
+		},
+		{
+			// Enforce Test
+			input: "http://instance1.geteduroam.nl/",
+			want:  "https://instance1.geteduroam.nl/",
+			e:     "",
+		},
+		{
+			// No URL
+			input: "foobar",
+			want:  "https://foobar",
+			e:     "",
+		},
+	}
+
+	for _, c := range cases {
+		p.Redirect = c.input
+		r, e := p.RedirectURI()
+		es := EtoString(e)
+		if r != c.want || es != c.e {
+			t.Fatalf("Result: %s, %s Want: %s, %s", r, es, c.want, c.e)
+		}
+	}
+}
