489bc450d9b8292a66d081e4dfe46b4d3bb6bcaa
Jeroen Wijenbergh <jeroenwijenbergh@protonmail.com>
2024-10-11 11:14:15 +0200

    EAP: Replace PKCS12 test data to be valid for 10 years
    Tests were giving expired :)

--- internal/eap/test_data/genpkcs12.sh
+++ internal/eap/test_data/genpkcs12.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p pkcs12
-openssl req -x509 -newkey rsa:4096 -keyout pkcs12/myKey.pem -out pkcs12/cert.pem -days 365 -nodes
+openssl req -x509 -newkey rsa:4096 -keyout pkcs12/myKey.pem -out pkcs12/cert.pem -days 3650 -nodes
 
 openssl pkcs12 -export -out pkcs12/container.p12 -inkey pkcs12/myKey.pem -in pkcs12/cert.pem
 
